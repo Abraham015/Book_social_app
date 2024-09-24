ce91ff6b8b45fc82e2217c712c4a2b454aa9ae76
AbrahamH015 <79383720+Abraham015@users.noreply.github.com>
2024-09-24 12:51:30 -0600

    Configure of the profiles and first commit

---
+++ book_social/src/test/java/dev/abraham/book/BookSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abraham.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
