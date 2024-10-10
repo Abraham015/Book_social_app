5aa7bea5a2fbdfc6a6855d2f7b388028d64c757f
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-10-10 09:28:03 -0600

    Changing docker file for frontend

---
+++ book-social-ui/dist/book-social-ui/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-M5ZY7YOF.mjs";import"./chunk-E64U5GZN.mjs";import"./chunk-LOR7YJIN.mjs";import"./chunk-HM557RPL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
