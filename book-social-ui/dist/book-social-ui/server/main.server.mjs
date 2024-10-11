a21c58981b224f3864c23865cda4784ac6cbf2f3
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-10-11 12:29:40 -0600

    new wchanges

--- book-social-ui/dist/book-social-ui/server/main.server.mjs
+++ book-social-ui/dist/book-social-ui/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7MWBOXAH.mjs";import"./chunk-36T4DESY.mjs";import"./chunk-E7QFFJ2L.mjs";import"./chunk-ZGAVEWIX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3M6PZI2C.mjs";import"./chunk-36T4DESY.mjs";import"./chunk-E7QFFJ2L.mjs";import"./chunk-ZGAVEWIX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- book-social-ui/src/app/app.routes.ts
+++ book-social-ui/src/app/app.routes.ts
@@ -1,9 +1,9 @@
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./pages/login/login.component";
-import {RegisterComponent} from "./pages/register/register.component";
-import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
-import {authGuard} from "./sevices/guard/auth.guard";
-import {NgModule} from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { authGuard } from './sevices/guard/auth.guard';
+import { NgModule } from '@angular/core';
 
 const routes: Routes = [
     {
@@ -20,14 +20,18 @@ const routes: Routes = [
     },
     {
         path: 'books',
-        loadChildren: ()=>import('./modules/book/book.module').then(m=>m.BookModule),
-        canActivate: [authGuard]
-    }
+        loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
+        canActivate: [authGuard],
+    },
+    // Add a fallback route if none of the routes match
+    {
+        path: '**',
+        redirectTo: 'login',
+    },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes || [])],
-    exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)], // Removed `|| []`
+    exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
