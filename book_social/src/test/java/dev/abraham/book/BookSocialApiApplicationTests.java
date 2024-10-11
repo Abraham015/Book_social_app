1c6b506faa7d7c44899b717b3861c885bfa2ff0a
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-10-10 22:07:30 -0600

    new changes

--- book_social/src/test/java/dev/abraham/book/BookSocialApiApplicationTests.java
+++ book_social/src/test/java/dev/abraham/book/BookSocialApiApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.abraham.book;
+/*package dev.abraham.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class BookSocialApiApplicationTests {
 	}
 
 }
+*/
