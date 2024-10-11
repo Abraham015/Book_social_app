c153ed4089af62e886eaea92b1eed39a8964b5ab
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-10-11 10:57:03 -0600

    new wchanges

--- book-social-ui/src/app/app.config.ts
+++ book-social-ui/src/app/app.config.ts
@@ -1,19 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { AppRoutingModule } from './app.routes';
+import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {httpTokenInterceptor} from "./sevices/interceptor/http-token.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-      provideZoneChangeDetection({ eventCoalescing: true }),
-      AppRoutingModule,
-      provideClientHydration(),
-      provideHttpClient(
-          withFetch(),
-          withInterceptors([httpTokenInterceptor])
-      )
-  ]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideClientHydration(),
+        provideHttpClient(
+            withFetch(),
+            withInterceptors([httpTokenInterceptor])
+        )
+    ]
 };
