6abf617afcb32543f838f68563ea56dfad93adc0
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-10-10 23:03:02 -0600

    new changes

--- book-social-ui/dist/book-social-ui/server/main.server.mjs
+++ book-social-ui/dist/book-social-ui/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-M5ZY7YOF.mjs";import"./chunk-E64U5GZN.mjs";import"./chunk-LOR7YJIN.mjs";import"./chunk-HM557RPL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MWWUI534.mjs";import"./chunk-E64U5GZN.mjs";import"./chunk-JVTTK432.mjs";import"./chunk-HM557RPL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
